bccc3ab787966c67a07334a5e9ff19170988193e
Yecheng Zhao <zyecheng@google.com>
2024-03-08 21:42:55 +0000

    Support waiting key stroke from console
    Implement an API for waiting a specific key stroke from the console.
    This will be primarily used for entering fastboot mode with `f` key
    press.
    
    Bug: 328786603
    Change-Id: I1625f81f964d278aeebdfcb48e641a2f21580a20

--- gbl/libefi/src/protocol.rs
+++ gbl/libefi/src/protocol.rs
@@ -227,6 +227,45 @@ impl From<core::fmt::Error> for EfiError {
     }
 }
 
+/// EFI_SIMPLE_TEXT_INPUT_PROTOCOL
+pub struct SimpleTextInputProtocol;
+
+impl ProtocolInfo for SimpleTextInputProtocol {
+    type InterfaceType = EfiSimpleTextInputProtocol;
+
+    const GUID: EfiGuid =
+        EfiGuid::new(0x387477c1, 0x69c7, 0x11d2, [0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b]);
+}
+
+impl Protocol<'_, SimpleTextInputProtocol> {
+    /// Wrapper of `EFI_SIMPLE_TEXT_INPUT_PROTOCOL.reset()`
+    pub fn reset(&self, extendend_verification: bool) -> EfiResult<()> {
+        // SAFETY:
+        // `self.interface()?` guarantees `self.interface` is non-null and points to a valid object
+        // established by `Protocol::new()`.
+        // `self.interface` is input parameter and will not be retained. It outlives the call.
+        unsafe { efi_call!(self.interface()?.reset, self.interface, extendend_verification) }
+    }
+
+    /// Wrapper of `EFI_SIMPLE_TEXT_INPUT_PROTOCOL.read_key_stroke()`
+    ///
+    /// Returns `Ok(Some(EfiInputKey))` if there is a key stroke, Ok(None) if no key stroke is
+    /// pressed.
+    pub fn read_key_stroke(&self) -> EfiResult<Option<EfiInputKey>> {
+        let mut key: EfiInputKey = Default::default();
+        // SAFETY:
+        // `self.interface()?` guarantees `self.interface` is non-null and points to a valid object
+        // established by `Protocol::new()`.
+        // `self.interface` is input parameter and will not be retained. It outlives the call.
+        // `key` is an output argument. It outlives the call and will not be taken.
+        match unsafe { efi_call!(self.interface()?.read_key_stroke, self.interface, &mut key) } {
+            Ok(()) => Ok(Some(key)),
+            Err(e) if e.is_efi_err(EFI_STATUS_NOT_READY) => Ok(None),
+            Err(e) => Err(e),
+        }
+    }
+}
+
 /// `EFI_DEVICE_PATH_PROTOCOL`
 pub struct DevicePathProtocol;
 
